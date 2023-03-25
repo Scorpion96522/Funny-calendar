3ed40686685728c3634129879815fb2d5b22d14b
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-03-25 19:13:56 +0300

    make const MonthNames, MonthLength, weekMonthDay

---
+++ main.js
@@ -0,0 +1,23 @@
+const dom = {
+    calendar: document.getElementById('calendar')
+
+}
+
+console.log(dom);
+
+const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const monthNames = [
+    'ЯНВАРЬ',
+    'ФЕВАРЛЬ',
+    'МАРТ',
+    'АПРЕЛЬ',
+    'МАЙ',
+    'ИЮНЬ',
+    'ИЮЛЬ',
+    'АВГУСТ',
+    'СЕНТЯБРЬ',
+    'ОКТЯБРЬ',
+    'НОЯБРЬ',
+    'ДЕКАБРЬ',
+]
+const weekMonthDays = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС',]
