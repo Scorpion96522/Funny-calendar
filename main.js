600a3cb07cae9dfad31378ef22bf73e395131fa1
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-03-29 22:58:19 +0300

    new object months

--- main.js
+++ main.js
@@ -5,27 +5,89 @@ const dom = {
 
 console.log(dom);
 
-const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-const monthNames = [
-    'ЯНВАРЬ',
-    'ФЕВАРЛЬ',
-    'МАРТ',
-    'АПРЕЛЬ',
-    'МАЙ',
-    'ИЮНЬ',
-    'ИЮЛЬ',
-    'АВГУСТ',
-    'СЕНТЯБРЬ',
-    'ОКТЯБРЬ',
-    'НОЯБРЬ',
-    'ДЕКАБРЬ',
+// const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+// const monthNames = [
+//     'ЯНВАРЬ',
+//     'ФЕВАРЛЬ',
+//     'МАРТ',
+//     'АПРЕЛЬ',
+//     'МАЙ',
+//     'ИЮНЬ',
+//     'ИЮЛЬ',
+//     'АВГУСТ',
+//     'СЕНТЯБРЬ',
+//     'ОКТЯБРЬ',
+//     'НОЯБРЬ',
+//     'ДЕКАБРЬ',
+// ]
+const months = [
+    {
+        title: 'Ёбаный <br>  новый год',
+        name: 'ЯНВАРЬ',
+        days: 31
+    },
+    {
+        title: 'Ёбаный <br> холод',
+        name: 'ФЕВАРЛЬ',
+        days: 28
+    },
+    {
+        title: 'Ёбаная <br> грязь',
+        name: 'МАРТ',
+        days: 31
+    },
+    {
+        title: 'Ёбаные <br> шутники',
+        name: 'АПРЕЛЬ',
+        days: 30
+    },
+    {
+        title: 'Ёбаный <br> день труда ',
+        name: 'МАЙ',
+        days: 31
+    },
+    {
+        title: 'Ёбаные <br> школьники',
+        name: 'ИЮНЬ',
+        days: 30
+    },
+    {
+        title: 'Ёбаная <br> жара',
+        name: 'ИЮЛЬ',
+        days: 31
+    },
+    {
+        title: 'Ёбаный <br> отпуск',
+        name: 'АВГУСТ',
+        days: 31
+    },
+    {
+        title: 'Ёбаное <br> 3 сентября ',
+        name: 'СЕНТЯБРЬ',
+        days: 30
+    },
+    {
+        title: 'Ёбаный <br> дождь ',
+        name: 'ОКТЯБРЬ',
+        days: 31
+    },
+    {
+        title: 'Ёбаные <br> скидки',
+        name: 'НОЯБРЬ',
+        days: 30
+    },
+    {
+        title: 'Ёбаные <br>  подарки ',
+        name: 'ДЕКАБРЬ',
+        days: 31
+    },
 ]
 const weekDayNames = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
 
 
-function renderMonth(monthNumber, year) {
-    const monthName = monthNames[monthNumber]
-    const monthHeadString = buildMonthHead('Заголовок', monthName)
+function renderMonth(monthIdx, year) {
+    const month = months[monthIdx]
+    const monthHeadString = buildMonthHead(month.title, month.name)
     const monthBox = document.createElement('div')
     monthBox.className = 'month'
     monthBox.innerHTML = monthHeadString
